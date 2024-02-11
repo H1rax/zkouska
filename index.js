a4ee16f3804cf9a84703e934b43b36356c767451
H1rax <kustkah@gmail.com>
2024-02-11 09:24:57 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+/*                1#  Console.log + Variables
+let jmeno = 'Petr';
+console.log(jmeno);
+let interestRate = 2;
+interestRate = 1;
+console.log(interestRate);
+*/
+
+
+/*                     Typy
+let name = 'Adam'; // String
+let age = 30; // Number
+let approved = true; // Boolean
+let first = null;
+*/
+
+
+
+/*                 2#  Objects
+let person = {
+    name: 'kerad',
+    age: 25
+}
+console.log(person)
+console.log(person.name, person.age)
+*/
+
+
+/*                 3# Arrays
+let selectedColors = ['Red', 'Blue']
+console.log(selectedColors[0])
+*/
+
+
+//                 4# funkce 
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName)
+}
+
+greet('John', 'Pete')
+greet('Marek', 'Hojer')
+
+
+
